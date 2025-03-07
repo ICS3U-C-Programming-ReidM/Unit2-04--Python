0c078f0976dda73129a7d2ffdea93d5e3e5589e0
Reid MacLean <reid.maclean@stu.ocsb.ca>
2025-03-07 17:50:40 +0000

    calculations for area and perimeter of a circle using math.pi

---
+++ area_per_circle.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+# Created by: Reid MacLean
+# Created on: March 7, 2025
+# This program calculates and displays the area and perimeter for a given user input
+
+import math
+
+rad = 6
+circ = math.pi * 2 * rad
+area = math.pi * rad**2
+print(circ, area)
